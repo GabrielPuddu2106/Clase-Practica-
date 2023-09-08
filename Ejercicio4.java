373b80d34ed1282d5ff89f59e277951cb360c6f9
GabrielPuddu2106 <142625919+GabrielPuddu2106@users.noreply.github.com>
2023-09-08 14:16:28 -0600

    commit 6

---
+++ Ejercicio4.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Ejercicio4{
+    public static void main(String [] args ){
+
+        Scanner lector = new Scanner(System.in);
+
+        double  a , b ,c ;
+
+        double  x1, x2, discriminante;
+
+        // DATOS DE ENTRADA
+
+        if ( a < = 0){
+        System.out.println("Ingrese el valor de a");
+        a = lector.nextDouble();
+        }  else {
+
+        System.out.println("Ingrese el valor de b");
+        b = lector.nextDouble();
+
+        System.out.println("Ingrese el valor de c");
+        c = lector.nextDouble();
+        }
+        // OPERACIONES 
+
+        discriminante = (b*b - 4*a*c);
+        x1 = ((-b) + Math.sqrt(discriminante)/ 2*a);
+        x2 = ((-b) - Math.sqrt(discriminante)/ 2*a);
+
+        if( discriminante = 0 ){
+            System.out.println(" Raiz 1 " +x1);
+
+        } else {
+            System.out.println(" Raiz 1 " +x1);
+            System.out.println(" Raiz 2 " +x2);
+
+        } 
+        lector.close();
+
+
+
+        
+    }
+}
